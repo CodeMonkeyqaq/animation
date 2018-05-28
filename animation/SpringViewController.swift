e9ac5a392f33ad4bc3f96e5b2f77b429d55b10c0
CodeMonkeyqaq <1242532947@qq.com>
2018-05-28 21:16:46 +0800

    add Spring and finished

---
+++ animation/SpringViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SpringViewController.swift
+//  animation
+//
+//  Created by 王星洲 on 2018/5/28.
+//  Copyright © 2018年 cn.edu.tongji.1652977. All rights reserved.
+//
+
+import UIKit
+
+class SpringViewController: UIViewController {
+    @IBOutlet weak var blueSquare: UIView!
+    
+    @IBOutlet weak var redSquare: UIView!
+    
+    @IBOutlet weak var greenSquare: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 1, animations: {
+            self.blueSquare.center.x = self.view.bounds.width - self.blueSquare.center.x
+        })
+        UIView.animate(withDuration: 5, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0, options: UIViewAnimationOptions.transitionCurlUp, animations: {
+            self.redSquare.center.x = self.view.bounds.width - self.redSquare.center.x
+        }, completion: nil)
+        UIView.animate(withDuration: 5, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 1, options: UIViewAnimationOptions.transitionCurlUp, animations: {
+            self.greenSquare.center.x = self.view.bounds.width - self.greenSquare.center.x
+        }, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
